d9a94b8207063dd37d2ab5e1670b9f7ef4f716b1
Sajal Prajapati <114945501+sajalprajapati@users.noreply.github.com>
2024-10-30 09:45:14 +0000

    object creation.

--- Learning the javascript/JavaScript Basics lecture2/index.js
+++ Learning the javascript/JavaScript Basics lecture2/index.js
@@ -13,6 +13,7 @@ console.log('How u doing');
 // console.log(person.name);
 
 // below is the object creation:
+/*
 let rectangle=
 {
     length:3,
@@ -31,4 +32,57 @@ console.log(rectangle['breadth']);
 console.log('The area is present below');
 console.log(rectangle['area']()); //this is how we call method of the object in the javascript.
 console.log(rectangle.area());
+
+*/
+//above type of programming we are is Object Oriented Programming.
+ 
+
+//to create object mutliple times ,we have two methods -- factory function and constructor function.
+
+//applying factory function:
+
+//writing it have two methods:
+
+//case 1:
+/*
+function createRectangle() //factory function 
+{
+    let rectangle=
+  {
+    length:3,
+    breadth:2,
+    area:function()
+    {
+        return this.length*this.breadth; //function is called as method in the object.
+    }
+   };
+
+   return rectangle;
+}
+*/
+
+
+//case 2:
+function createRectangle(x, y) //factory function 
+{
+    return rectangle=
+  {
+
+     length:x,
+     breadth:y,
+     area()
+     {
+        return this.length*this.breadth; //function is called as method in the object.
+     }
+
+   };
+
+}
+
+let x=prompt("Enter the value of x:");
+let y=prompt("Enter the value of y:");
+
+let rectangleObj1= createRectangle(x,y);
+console.log(rectangleObj1['area']());
+
 
