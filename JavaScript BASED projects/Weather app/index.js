e86eccb04cfc3f8691d65e3cf013f4549f2eb738
Sajal Prajapati <114945501+sajalprajapati@users.noreply.github.com>
2024-11-20 06:35:52 +0000

    Learning the API and Async function .

--- JavaScript BASED projects/Weather app/index.js
+++ JavaScript BASED projects/Weather app/index.js
@@ -8,22 +8,31 @@
  const API_KEY="d1845658f92b31c64bd94f06f7188c9c";
 async function showWeather()
 {
-    let city="goa";
+    try{ let city="goa";
    
 
-    const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
-
-    const data= await response.json();
-
-    console.log("Weather is",data);
-    // console.log(data.weather[0]);
+        const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
     
-
-    let newPara=document.createElement('p');
-    newPara.textContent=`${data?.main?.temp.toFixed(2)} C`;
-    document.body.appendChild(newPara);
+        const data= await response.json();
+    
+        console.log("Weather is",data);
+      
+        showingData(data);
+    }
+    catch(err)
+    {
+        newPara.textContent='404';
+        document.body.appendChild(newPara);
+    }
+   
 }
 
 
+async function showingData(data)
+{
+
+    let newPara=document.createElement('p');
+    newPara.textContent=`${data?.main?.temp.toFixed(2)} C`;
+    document.body.appendChild(newPara);}
 
 showWeather();
